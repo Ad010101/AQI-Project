774a9e0a83f246fc677e695a2ada2bebbb90c8d5
Ad010101 <96282072+Ad010101@users.noreply.github.com>
2023-10-31 16:52:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,url_for,request
 import pandas as pd 
 import pickle
-loaded_model=pickle.load(open('XgBoost.pkl', 'rb'))
+loaded_model=pickle.load(open('regression_model.pkl', 'rb'))
 app = Flask(__name__)
 
 @app.route('/')
