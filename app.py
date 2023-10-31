bcecd39bd4431ee2e427a161934f77a1743b5df8
Ad010101 <96282072+Ad010101@users.noreply.github.com>
2023-10-31 10:26:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,url_for,request
 import pandas as pd 
 import pickle
-loaded_model=pickle.load(open('regression_model.pkl', 'rb'))
+loaded_model=pickle.load(open('XgBoost.pkl', 'rb'))
 app = Flask(__name__)
 
 @app.route('/')
@@ -20,32 +20,39 @@ def predict():
 
     my_prediction=loaded_model.predict(df.iloc[:,:-1].values)
     my_prediction=my_prediction.tolist()
-    
-    if(int(month)==1):
-       element = my_prediction[0:31]
-    if(int(month)==2):
-       element = my_prediction[31:59]
-    if(int(month)==3):
-       element = my_prediction[59:90]
-    if(int(month)==4):
-       element = my_prediction[90:120]
-    if(int(month)==5):
-       element = my_prediction[120:151]
-    if(int(month)==6):
-       element = my_prediction[151:181]
-    if(int(month)==7):
-       element = my_prediction[181:212]
-    if(int(month)==8):
-       element = my_prediction[212:243]
-    if(int(month)==9):
-       element = my_prediction[243:273]
-    if(int(month)==10):
-       element = my_prediction[273:303]
-    if(int(month)==11):
-       element = my_prediction[303:334]
-    if(int(month)==12):
-       element = my_prediction[334:365]
-        
+    month = int(month)  # Assuming you already have the month and year defined
+    day_ranges = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 303, 334]
+    if(year!=2016):
+        month = int(month)  # Assuming you already have the month and year defined
+
+        days_in_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+        if 1 <= month <= 12:
+            start_day = sum(days_in_month[:month])
+            end_day = start_day + days_in_month[month]
+
+            if year != 2016:
+                if month == 2:
+                    start_day += 1  # Adjust for leap year
+                    end_day += 1
+
+            element = my_prediction[start_day:end_day]
+        else:
+            print("Invalid month")
+    else:
+        days_in_month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+        if 1 <= month <= 12:
+            start_day = sum(days_in_month[:month])
+            end_day = start_day + days_in_month[month]
+
+            if year == 2016 and month == 2:
+                
+                end_day += 1
+
+            element = my_prediction[start_day:end_day]
+
+
     return render_template('result.html',prediction = element)
 if __name__ == '__main__':
 	app.run(debug=True)
